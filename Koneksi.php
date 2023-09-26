7e175b103a3abefd665d7b7aa401133e6ee1d2b0
Tim7PPlg <144772156+Tim7PPlg@users.noreply.github.com>
2023-09-26 13:52:11 +0700

    Update Koneksi.php

--- Koneksi.php
+++ Koneksi.php
@@ -1,1 +1,8 @@
+<?php
+$koneksi =mysqli_connect("localhost","root","","db_website");
 
+// check connection
+if (mysqli_connect_errno()){
+   echo "koneksi database gagal : " . mysqli_connect_error();
+}
+?>
