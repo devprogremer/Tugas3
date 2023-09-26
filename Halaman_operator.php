a61864ffb5b9dbd4fa607c71185583956fff03d1
Tim7PPlg <144772156+Tim7PPlg@users.noreply.github.com>
2023-09-26 14:09:48 +0700

    Update Halaman_operator.php

--- Halaman_operator.php
+++ Halaman_operator.php
@@ -1,1 +1,28 @@
+<! DOCTYPE html>
+<html>
+<head>
+  <title>Halaman Operator</title>
+</head>
+<body>
+   <?php
+    session_start();
 
+   // cek apakah yang mengakses halasan ini sudah login 
+if ($_SESSION['level']==""){
+ header("location:index.php?pesan-pagal");
+ }
+
+?>
+<h1>halaman Pengurus</h1>
+
+<p>Halo <b><?php echo $_SESION['username']; 
+?></b> Anda telah login sebagai <b>
+<?php echo $_SESSION['level'];
+?></b>.</p>
+<a href="logout.php">LOGOUT</a>
+
+<br/>
+<br/>
+<a>Membuat Multi Level Dengan PHP</a>
+</body>
+</html>
